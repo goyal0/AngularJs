3b32db9ee2d9333f4c87b5d8f6616c32cd463c66
piyush goyal <piygoyal30@gmail.com>
2025-03-07 13:34:59 +0530

    [Piyush Goyal] opening website on next page

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
-  imgUrl="src\assets\images.jpg";
+  imgUrl="../assets/images.jpg";
+  url="https://www.bridgelabz.com/"
 
   ngOnInit():void{
     this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event:Event){
+    console.log("Save button is clicked", $event);
+    window.open(this.url, "_blank");
   }
 }
